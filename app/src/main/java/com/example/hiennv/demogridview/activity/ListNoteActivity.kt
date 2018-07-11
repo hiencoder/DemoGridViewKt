449b8639460512a3710403c47f3b5040d2cef214
hiencoder <mrphonglinh2605@gmail.com>
2018-07-11 10:11:59 +0700

    Commit activity

---
+++ app/src/main/java/com/example/hiennv/demogridview/activity/ListNoteActivity.kt
@@ -0,0 +1,13 @@
+package com.example.hiennv.demogridview.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.hiennv.demogridview.R
+
+class ListNoteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_note)
+    }
+}
